ce9ff39edc232777984eaa536194b828fc03a155
Adwait <adwait.kul.2018@gmail.com>
2023-03-01 14:25:49 -0800

    add styling for nav elements

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,26 +2,28 @@ import logo from "../assets/Logo.svg";
 
 export default function Nav() {
   return (
-    <nav>
-      <img src={logo} alt="logo"></img>
-      <li>
-        <a href="/Home">Home</a>
-      </li>
-      <li>
-        <a href="/Menu">Menu</a>
-      </li>
-      <li>
-        <a href="/Login">Login</a>
-      </li>
-      <li>
-        <a href="/Reservation">Reservation</a>
-      </li>
-      <li>
-        <a href="/OrderOnline">Order Online</a>
-      </li>
-      <li>
-        <a href="/About">About Us</a>
-      </li>
+    <nav className="nav">
+      <ul className="navli">
+        <img className=".logo" src={logo} alt="logo"></img>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+        <li>
+          <a href="/reservation">Reservation</a>
+        </li>
+        <li>
+          <a href="/orderOnline">Order Online</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+      </ul>
     </nav>
   );
 }
