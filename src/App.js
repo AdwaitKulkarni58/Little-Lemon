d49e53d504bd92dd8cca3fd3d4a7db86d8439652
Adwait <adwait.kul.2018@gmail.com>
2023-03-01 12:20:56 -0800

    make individual semantic components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div>Homepage</div>;
+  return (
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <Main>Homepage</Main>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
