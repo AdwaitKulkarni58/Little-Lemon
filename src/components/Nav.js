0500c52efcc7f7210bf7e353afd8c5d0b75f883b
Adwait <adwait.kul.2018@gmail.com>
2023-03-01 13:43:21 -0800

    add nav component links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,27 @@
+import logo from "../assets/Logo.svg";
+
 export default function Nav() {
-  return <Nav></Nav>;
+  return (
+    <nav>
+      <img src={logo} alt="logo"></img>
+      <li>
+        <a href="/Home">Home</a>
+      </li>
+      <li>
+        <a href="/Menu">Menu</a>
+      </li>
+      <li>
+        <a href="/Login">Login</a>
+      </li>
+      <li>
+        <a href="/Reservation">Reservation</a>
+      </li>
+      <li>
+        <a href="/OrderOnline">Order Online</a>
+      </li>
+      <li>
+        <a href="/About">About Us</a>
+      </li>
+    </nav>
+  );
 }
